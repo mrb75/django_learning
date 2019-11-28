67daca238923d27c24a0b7e4ed8cebaa36270ade
mrb76 <mrbakhtiari4@yahoo.com>
2019-11-28 19:19:23 +0330

    write minimal form

--- polls/urls.py
+++ polls/urls.py
@@ -5,5 +5,7 @@ from . import views
 from polls.models import Question
 
 urlpatterns = [
-    path('',views.index,name='index'),
+    path('', views.index, name='index'),
+    path('detail/<int:question_id>', views.detail, name='detail'),
+    path('vote/<int:question_id>',views.vote,name='vote'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse,Http404
+from django.http import HttpResponse,Http404,HttpResponseRedirect
 from django.template import loader
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
 
-from .models import Question
+from .models import Question,Choice
 
 # Create your views here.
 def index(request):
@@ -13,4 +14,24 @@ def index(request):
     #     'last_question':last_questuion
     # }
     # return HttpResponse(template.render(context,request))
-    return render(request,'polls/resources/index.html',{"last_questuion":last_questuion})
+    return render(request, 'polls/resources/index.html', {"last_questuion": last_questuion})
+def detail(request, question_id):
+    # try:
+    #     question = Question.objects.get(pk=question_id)
+    # except Question.DoesNotExist:
+    #     raise Http404("question does not found!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+    question=get_object_or_404(Question,pk=question_id)
+    return render(request, 'polls/resources/question.html', {"question": question})
+def vote(request, question_id):
+    question=get_object_or_404(Question,pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/resources/index.html', {
+            'question': question,
+            'errorMessage':'do not vote',
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('index'))
